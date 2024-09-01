a101dc729d64b6bf052bbdacdbd8a8cba89107b0
Joshkindled1 <Leeyuhong1@gmail.com>
2024-09-01 15:00:20 +0800

    assessement done

--- k_assessment_structure_v3_skeleton/index.js
+++ k_assessment_structure_v3_skeleton/index.js
@@ -5,90 +5,165 @@ const GRASS = "░";
 const HOLE = "O";
 const CARROT = "^";
 const PLAYER = "*";
+const OUTB = undefined;
 
 // WIN / LOSE / OUT / QUIT messages constants
-const WIN = "";                                                                 // TODO: customise message when player wins
-const LOST = "";                                                                // TODO: customise message when player lose
-const OUT = "";                                                                 // TODO: customise message when player is out of bounds (lose)
+const WIN = "**************************************************\n You win - You found the carrot!";                                                                 // TODO: customise message when player wins
+const LOST = "**************************************************\n You lost - You droppped into a hole!";                                                                // TODO: customise message when player lose
+const OUT = "**************************************************\n You lost-You are out of boundary";                                                                 // TODO: customise message when player is out of bounds (lose)
 const QUIT = "Game Ended."                                                      // TODO: customise message when player quits
 
-class Field{
+const rows = 8;                                                                 // the game map will have 8 rows
+const cols = 5;                                                                 // the game map will have 5 cols
+const percentage = .2;                                                          // % of holes for the map
 
-    // TODO: create the constructor
 
-    static welcomeMsg(msg){                                                     // static Method to show game's welcome message
+class Field {
+
+
+    constructor(field = [[]]) {
+        this.field = field;          //this.field is a property of the class Field
+        this.gamePlay = false;          // starts as false before the game starts
+        this.a = 0;
+        this.b = 0;
+    }
+
+
+    static welcomeMsg(msg) {                                                     // static Method to show game's welcome message
+        console.log(msg);
+    }
+
+    static generateField(rows, cols, percentage) {                              //static method that generates and return a 2Dmap
         // TODO:
+        const map = [[]];
+        for (let i = 0; i < rows; i++) {
+            map[i] = [];
+            for (let j = 0; j < cols; j++) {
+                map[i][j] = Math.random() > percentage ? GRASS : HOLE;          //
+            }
+
+        }
+        return map;
     }
 
-    static generateField(rows, cols, percentage) {   
+    printField() {                                                               // print the game field (used to update during gameplay)       
         // TODO:
+        this.field.forEach(element => {
+
+            console.log(element.join(""));
+        }
+        )
     }
 
-    printField(){                                                               // print the game field (used to update during gameplay)       
-        // TODO:
+    updateGame(input) {                                                          // TODO: Refer to details in the method's codeblock
+
+        const userInput = String(input).toLowerCase();
+
+        switch (this.field[this.a][this.b]) {
+            case HOLE:
+                console.log(LOST);
+                this.gamePlay = false;
+                break;
+            case CARROT:
+                console.log(WIN);
+                this.gamePlay = false;
+                break;
+            case GRASS:
+                console.log('Keep looking for the hat...');
+                this.field[this.a][this.b] = PLAYER;
+                break;
+            case PLAYER:
+                console.log('You are stepping on *');
+                break;
+            case OUTB:
+                this.gamePlay = false;
+                console.log(OUT);
+                break;
+            }
+       
+
+       
+            if (userInput === 'q') {
+            this.quitGame();
+        }
+
     }
 
-    updateGame(input){                                                          // TODO: Refer to details in the method's codeblock
-
-      const userInput = String(input).toLowerCase();
-    
-        /*   
-        TODO: if the user arrives at the carrot
-        end the game - set gamePlay = false;
-        inform the user that he WIN the game 
-        */
-
-        /* 
-        TODO: if the user arrives at the hole
-        end the game - set the gamePlay = false;
-        inform the user that he LOST the game
-        */
-
-        /*  
-        TODO: if the user exits out of the field
-        end the game - set the gamePlay = false;
-        inform the user that he step OUT of the game
-        */
-
-        /*  
-        TODO: if the user ends the game
-        end the game - set the gamePlay = false;
-        inform the user that he QUIT the game
-        */
-
-
-        /* 
-        TODO: otherwise, move player on the map: field[rowindex][colindex] = CARROT;
-        update the display to show the user had moved to the new area on map
-        ask for player's next move as well 
-        */
+    plantCarrot() {
+        const X = Math.floor(Math.random() * (rows - 1) + 1);
+        const Y = Math.floor(Math.random() * (cols - 1) + 1);
+        this.field[X][Y] = CARROT;
     }
 
-    startGame(){                                                                
-        // TODO:
+    startGame() {
+        this.gamePlay = true;                           // set this.gamePlay = true; to keep the game running
+
+        this.field[0][0] = PLAYER;
+
+        this.plantCarrot();
+
+        while (this.gamePlay) {
+
+            this.printField();
+
+            let flagInvalid = false;
+            const input = prompt("**************************************************\nWhich Way:");       //obtain the user's direction ( up down left of right)
+            console.log("(u)p, (d)own, (l)eft, (r)ight, (q)uit"); //provide instruction for player
+
+            switch (input.toLowerCase()) {                         //feedback user's input
+                case "u":
+                    console.log("**************************************************\nYou moved up!");
+                    this.a -= 1;
+                    break;
+                case "d":
+                    console.log("**************************************************\nYou moved down!");
+                    this.a += 1;
+                    break;
+                case "l":
+                    console.log("**************************************************\nYou moved left");
+                    this.b -=1;
+                    break;
+                case "r":
+                    console.log("**************************************************\nYou moved right");
+                    this.b +=1;
+                    break;
+                case "q":
+                    console.log("**************************************************\nquit");
+
+                    break;
+                default:
+                    console.log("**************************************************\nInvalid input");
+                    flagInvalid = !flagInvalid;
+                    break;
+            }
+
+            if (!flagInvalid) {
+                this.updateGame(input);
+            }
+
+        }
     }
 
-    endGame(){                                                                  
+    endGame() {
         this.gamePlay = false;                                                  // set property gamePlay to false
-        process.exit();
+        process.exit();                                                         //ends the node app
     }
 
-    quitGame(){
+    quitGame() {
         console.log(QUIT);
         this.endGame();
     }
 
 }
 
-const rows = 8;                                                                 // the game map will have 8 rows
-const cols = 5;                                                                 // the game map will have 5 cols
-const percentage = .2;                                                          // % of holes for the map
+
 
 // Instantiate a new instance of Field Class
-const createField = Field.generateField(rows, cols, percentage);
+const createField = Field.generateField(rows, cols, percentage);                //call Field's class static method to generate 2d field
+
 const gameField = new Field(createField);
 
 Field.welcomeMsg("Welcome to Find Your Hat!\n**************************************************\\n");
 
 gameField.startGame();
-gameField.printField();                                                         // to test only
+
